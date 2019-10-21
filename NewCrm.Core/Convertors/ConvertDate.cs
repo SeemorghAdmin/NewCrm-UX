ce4f4cc05d2d251bfb271fa028cf37ada8fc00c5
rezanouri1993 <rezanouri1993@outlook.com>
2019-10-21 12:30:24 +0330

    data

---
+++ NewCrm.Core/Convertors/ConvertDate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace NewCrm.Core.Convertors
+{
+    public class ConvertDate
+    {
+        public static string ConvertSha(DateTime dateTime)
+        {
+            PersianCalendar pc = new PersianCalendar();
+
+            string da = $"Persian Calendar:    { pc.GetDayOfWeek(dateTime)}, { pc.GetMonth(dateTime)}/{pc.GetDayOfMonth(dateTime)}" +
+                $"/{ pc.GetYear(dateTime)} {pc.GetHour(dateTime)}:{ pc.GetMinute(dateTime) }:{pc.GetSecond(dateTime)}";
+                       
+            return da;
+        }
+
+        public static DateTime ConvetMil(DateTime year)
+        {
+            PersianCalendar pc = new PersianCalendar();
+
+            DateTime dateTime = new DateTime(pc.GetYear(year), 1, 1);
+
+            return dateTime;
+        }
+    }
+}
